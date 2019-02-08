8a060a5b35b134870d75604bc69c43ec4169c8e9
Dadoudidou <davidviolet18@gmail.com>
2019-02-08 16:30:36 +0100

    Make User service first commit

---
+++ services/users/webpack.config.js
@@ -0,0 +1,43 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+    entry: './src/index.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: [
+            '.tsx',
+            '.ts',
+            '.js'
+        ]
+    },
+    plugins: [
+        new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en/),
+    ],
+    optimization: {
+        runtimeChunk: 'single',
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\\/]node_modules[\\\/]/,
+                    name: 'vendors',
+                    chunks: 'all'
+                }
+            }
+        }
+    }
+}
+
+module.exports = config;
